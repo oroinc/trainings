813201ead25bef05a659f6c818f765f83c6f8995
Zaklina Data <zdata@oroinc.com>
2022-02-24 10:50:42 +0100

    Step2. Entities and migrations: Practice solution

--- src/Training/Bundle/UserNamingBundle/Provider/EntityNameProviderDecorator.php
+++ src/Training/Bundle/UserNamingBundle/Provider/EntityNameProviderDecorator.php
@@ -4,35 +4,45 @@ namespace Training\Bundle\UserNamingBundle\Provider;
 
 use Oro\Bundle\EntityBundle\Provider\EntityNameProviderInterface;
 use Oro\Bundle\UserBundle\Entity\User;
+use Training\Bundle\UserNamingBundle\Entity\UserNamingType;
 
 class EntityNameProviderDecorator implements EntityNameProviderInterface
 {
-    /** @var EntityNameProviderInterface */
-    private $originalProvider;
+    private EntityNameProviderInterface $originalProvider;
+    private UserFullNameProvider $fullNameProvider;
 
     /**
      * @param EntityNameProviderInterface $originalProvider
+     * @param UserFullNameProvider $fullNameProvider
      */
-    public function __construct(EntityNameProviderInterface $originalProvider)
-    {
+    public function __construct(
+        EntityNameProviderInterface $originalProvider,
+        UserFullNameProvider $fullNameProvider
+    ) {
         $this->originalProvider = $originalProvider;
+        $this->fullNameProvider = $fullNameProvider;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getName($format, $locale, $entity)
+    public function getName($format, $locale, $entity): string
     {
         if ($entity instanceof User) {
-            return sprintf('%s %s %s', $entity->getLastName(), $entity->getFirstName(), $entity->getMiddleName());
+            /** @var UserNamingType|null $namingType */
+            $namingType = $entity->getNamingType();
+            if ($namingType) {
+                return $this->fullNameProvider->getFullName($entity, $namingType->getFormat());
+            }
         }
+
         return $this->originalProvider->getName($format, $locale, $entity);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getNameDQL($format, $locale, $className, $alias)
+    public function getNameDQL($format, $locale, $className, $alias): string
     {
         return $this->originalProvider->getNameDQL($format, $locale, $className, $alias);
     }
