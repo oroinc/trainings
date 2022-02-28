7051610549e786196b7beb5038dd0e3767e3d180
Yevhen Shyshkin <yshyshkin@oroinc.com>
2022-02-28 17:16:10 +0100

    Step 1: Themes and Layouts

---
+++ src/Training/Bundle/FirstThemeBundle/Layout/DataProvider/WeatherDataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Training\Bundle\FirstThemeBundle\Layout\DataProvider;
+
+class WeatherDataProvider
+{
+    /**
+     * @return string
+     */
+    public function getWeatherText()
+    {
+        return 'Current weather is OK.';
+    }
+}
---
+++ src/Training/Bundle/FirstThemeBundle/TrainingFirstThemeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Training\Bundle\FirstThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TrainingFirstThemeBundle extends Bundle
+{
+}
