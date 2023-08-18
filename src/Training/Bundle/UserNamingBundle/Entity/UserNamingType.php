febe4a8a2663b5df0a28361dcd2eb85d9a2c8789
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2023-08-18 10:34:08 +0200

    fundamentals_step_2_entities_migrations

---
+++ src/Training/Bundle/UserNamingBundle/Entity/UserNamingType.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Training\Bundle\UserNamingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+use Oro\Bundle\EntityExtendBundle\Entity\ExtendEntityInterface;
+use Oro\Bundle\EntityExtendBundle\Entity\ExtendEntityTrait;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="training_user_naming_type")
+ * @Config(
+ *      defaultValues={
+ *          "entity"={
+ *              "icon"="fa-child"
+ *          }
+ *      }
+ * )
+ */
+class UserNamingType implements ExtendEntityInterface
+{
+    use ExtendEntityTrait;
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer", name="id")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=64, nullable=false)
+     */
+    private $title;
+
+    /**
+     * @var string
+     * Allowed placeholders are: PREFIX, FIRST, MIDDLE, LAST, SUFFIX
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $format;
+
+    /**
+     * @return int|null
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * @param string $format
+     * @return $this
+     */
+    public function setFormat(string $format): self
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+}
--- src/Training/Bundle/UserNamingBundle/Provider/EntityNameProviderDecorator.php
+++ src/Training/Bundle/UserNamingBundle/Provider/EntityNameProviderDecorator.php
@@ -4,31 +4,40 @@ namespace Training\Bundle\UserNamingBundle\Provider;
 
 use Oro\Bundle\EntityBundle\Provider\EntityNameProviderInterface;
 use Oro\Bundle\UserBundle\Entity\User;
+use Training\Bundle\UserNamingBundle\Entity\UserNamingType;
 
 class EntityNameProviderDecorator implements EntityNameProviderInterface
 {
     /**
      * @param EntityNameProviderInterface $originalProvider
+     * @param UserFullNameProvider $fullNameProvider
      */
-    public function __construct(private EntityNameProviderInterface $originalProvider)
-    {
+    public function __construct(
+        private EntityNameProviderInterface $originalProvider,
+        private UserFullNameProvider $fullNameProvider
+    ) {
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getName($format, $locale, $entity)
+    public function getName($format, $locale, $entity): string
     {
         if ($entity instanceof User) {
-            return sprintf('%s %s %s', $entity->getLastName(), $entity->getFirstName(), $entity->getMiddleName());
+            /** @var UserNamingType|null $namingType */
+            $namingType = $entity->getNamingType();
+            if ($namingType) {
+                return $this->fullNameProvider->getFullName($entity, $namingType->getFormat());
+            }
         }
+
         return $this->originalProvider->getName($format, $locale, $entity);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getNameDQL($format, $locale, $className, $alias)
+    public function getNameDQL($format, $locale, $className, $alias): string
     {
         return $this->originalProvider->getNameDQL($format, $locale, $className, $alias);
     }
