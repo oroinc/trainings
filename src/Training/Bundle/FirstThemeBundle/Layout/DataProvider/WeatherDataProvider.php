47ca72b88a71822023285f7b773cb20722bc3b3d
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2023-08-18 09:16:31 +0200

    Step1: Themes and Layouts

---
+++ src/Training/Bundle/FirstThemeBundle/Layout/DataProvider/WeatherDataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Training\Bundle\FirstThemeBundle\Layout\DataProvider;
+
+class WeatherDataProvider
+{
+    /**
+     * @return string
+     */
+    public function getWeatherText(): string
+    {
+        return 'Current weather is OK.';
+    }
+}
