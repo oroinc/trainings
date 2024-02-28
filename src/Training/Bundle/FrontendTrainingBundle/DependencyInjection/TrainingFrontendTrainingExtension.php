b75ee3414fd9f35160ab206cd955458352b12758
Maciej Garycki <mgarycki@oroinc.com>
2024-02-28 11:29:59 +0100

    EDU-31: Global/JS configuration - bundle structure - added backend config - added js global config

---
+++ src/Training/Bundle/FrontendTrainingBundle/DependencyInjection/TrainingFrontendTrainingExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Training\Bundle\FrontendTrainingBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class TrainingFrontendTrainingExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+
+        $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
+    }
+}
