5ee70c10b7597bdf45abfa07f60ddc93141fd27a
Daniel Nahrebecki <dnahrebecki@oroinc.com>
2024-03-08 14:33:04 +0100

    EDU-33: Refreshing of pricing layout block (PLP)

---
+++ src/Training/Bundle/FrontendTrainingBundle/Resources/public/js/app/components/layout-block-refresh-component.js
@@ -0,0 +1,52 @@
+import BaseComponent from 'oroui/js/app/components/base/component';
+import moduleConfig from 'module-config';
+import LayoutSubtreeView from 'oroui/js/app/views/layout-subtree-view';
+import mediator from 'oroui/js/mediator';
+
+const config = {
+    ...moduleConfig(module.id)
+};
+
+const LayoutBlockRefreshComponent = BaseComponent.extend({
+    $el: null,
+
+    constructor: function LayoutBlockRefreshComponent(options) {
+        LayoutBlockRefreshComponent.__super__.constructor.call(this, options);
+    },
+
+    initialize(options) {
+        this.$el = options._sourceElement;
+
+        // if there is no config.datagridRefreshSec set, do not initialize this component
+        if (!config.datagridRefreshSec) {
+            return;
+        }
+
+        // check if options.refreshBlockId and options.reloadEvents are set, if not, throw error
+        if (!options.refreshBlockId || !options.reloadEvents) {
+            throw new Error('refreshBlockId and reloadEvents are required options in layout-block-refresh-component');
+        }
+
+        new LayoutSubtreeView({
+            el: this.$el,
+            blockId: options.refreshBlockId,
+            reloadEvents: options.reloadEvents,
+            restoreFormState: true
+        });
+
+        // call mediator trigger to refresh the block every x seconds (from config)
+        setInterval(() => {
+            mediator.trigger('training-product-datagrid:refresh');
+        }, config.datagridRefreshSec * 1000);
+    },
+
+    dispose: function() {
+        if (this.disposed) {
+            return;
+        }
+
+        LayoutBlockRefreshComponent.__super__.dispose.call(this);
+    }
+});
+
+export default LayoutBlockRefreshComponent;
