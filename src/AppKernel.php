7a09b5ead86400d1c469f2c036d8fbb325ff436d
veremenko <veremenko@oroinc.com>
2024-04-24 19:06:16 +0300

    add new theme 6.0

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -11,11 +11,16 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class AppKernel extends OroKernel
 {
-    public function registerBundles()
+    public function registerBundles(): iterable
     {
         $bundles = array(
             // bundles
         );
+
+        if ($this->isDebug()) {
+            ini_set('memory_limit', -1);
+            ini_set('max_execution_time', 0);
+        }
 
         if ('dev' === $this->getEnvironment()) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
@@ -49,7 +54,7 @@ class AppKernel extends OroKernel
     /**
      * {@inheritdoc}
      */
-    public function getCacheDir()
+    public function getCacheDir(): string
     {
         return dirname(__DIR__).'/var/cache/'.$this->environment;
     }
@@ -57,7 +62,7 @@ class AppKernel extends OroKernel
     /**
      * {@inheritdoc}
      */
-    public function getLogDir()
+    public function getLogDir(): string
     {
         return dirname(__DIR__).'/var/logs';
     }
