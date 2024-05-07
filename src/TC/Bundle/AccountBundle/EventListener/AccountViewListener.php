1d1c47bbe1cf12ecc2b8a7a6372b9e84cb28ece2
Żaklina Data <zaklina-data@users.noreply.github.com>
2024-05-07 11:49:46 +0200

    EDU-44 Backoffice UI/UX - Overriding content (#59)
    * EDU-44 Backoffice UI/UX - Overriding content
    ---------
    
    Co-authored-by: Żaklina Data <zdata@oroinc.com>
    Co-authored-by: Marcin Łukaszewicz <mlukaszewicz@oroinc.com>

---
+++ src/TC/Bundle/AccountBundle/EventListener/AccountViewListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace TC\Bundle\AccountBundle\EventListener;
+
+use Oro\Bundle\UIBundle\Event\BeforeListRenderEvent;
+use Oro\Bundle\AccountBundle\Entity\Account;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use TC\Bundle\AccountBundle\Helper\CustomerFinder;
+
+class AccountViewListener
+{
+    public function __construct(
+        private CustomerFinder $customerFinder,
+        private TranslatorInterface $translator
+    ) {
+    }
+
+    public function onView(BeforeListRenderEvent $event): void
+    {
+        /** @var Account $account */
+        $account = $event->getEntity();
+        if (!$account) {
+            return;
+        }
+
+        $customers = $this->customerFinder->getRelatedToAccounts([$account]);
+        $template = $event->getEnvironment()->render(
+            '@TCAccount/Account/customersAddresses.html.twig',
+            ['customers' => $customers]
+        );
+
+        $blockId = $event->getScrollData()->addBlock(
+            $this->translator->trans('tc.account.block.addresses'),
+            1
+        );
+        $subBlockId = $event->getScrollData()->addSubBlock($blockId);
+
+        $event->getScrollData()->addSubBlockData($blockId, $subBlockId, $template);
+    }
+}
