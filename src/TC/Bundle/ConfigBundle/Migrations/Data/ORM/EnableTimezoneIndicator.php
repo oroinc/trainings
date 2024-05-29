40ec49eff20802916f1dcdb9bca5d9817246bf0e
Żaklina Data <zaklina-data@users.noreply.github.com>
2024-05-29 12:06:42 +0200

    EDU-46 Configuration - System Config and env variables (#64)
    * EDU-46 Configuration - System Config and env variables
    
    * EDU-46 Configuration - System Config and env variables
    
    - fixed typo
    
    * EDU-46 Configuration - System Config and env variables
    - template 'copy paste' fix
    
    ---------
    
    Co-authored-by: zaklina <zdata@oroinc.com>
    Co-authored-by: Marcin Łukaszewicz <mlukaszewicz@oroinc.com>

---
+++ src/TC/Bundle/ConfigBundle/Migrations/Data/ORM/EnableTimezoneIndicator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TC\Bundle\ConfigBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Persistence\ObjectManager;
+use Oro\Bundle\UserBundle\DataFixtures\UserUtilityTrait;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use TC\Bundle\ConfigBundle\DependencyInjection\Configuration;
+
+class EnableTimezoneIndicator extends AbstractFixture implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+    use UserUtilityTrait;
+
+    public function load(ObjectManager $manager)
+    {
+        $configManager = $this->container->get('oro_config.global');
+
+        $configManager->set(Configuration::getConfigKey(Configuration::CONFIG_TIMEZONE_INDICATOR_ENABLED), true);
+        $configManager->flush();
+
+        // per user
+        $user = $this->getFirstUser($manager);
+
+        $userConfigManager = $this->container->get('oro_config.user');
+        $userConfigManager->setScopeId($user->getId());
+
+        $userConfigManager->set(
+            Configuration::getConfigKey(Configuration::CONFIG_TIMEZONE_INDICATOR_ENABLED),
+            true,
+            $user->getId()
+        );
+        $userConfigManager->flush();
+    }
+}
