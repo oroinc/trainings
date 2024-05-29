9e25371ba000e16946e7eeaed3f05d6a3ced8c67
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2024-05-29 10:27:39 +0200

    EDU-46 Configuration - System Config and env variables
    - fixed typo

--- src/TC/Bundle/ConfigBundle/Twig/ParametersExtension.php
+++ src/TC/Bundle/ConfigBundle/Twig/ParametersExtension.php
@@ -6,13 +6,13 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
-class ParamentersExtension extends AbstractExtension
+class ParametersExtension extends AbstractExtension
 {
     public function __construct(protected ParameterBagInterface $params)
     {
     }
 
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new TwigFunction('get_parameter_value', [$this, 'getParameterValue']),
