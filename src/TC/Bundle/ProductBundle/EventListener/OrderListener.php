56b69a0dbb02ce1b37585d696f30423378ce8c1b
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2024-05-29 10:40:39 +0200

    EDU-56: Website Search - add new product field to index (#67)
    * EDU-56: Website Search - add new product field to index
    - implementation
    
    * EDU-56: Website Search - add new product field to index
    - small fixes
    
    ---------
    
    Co-authored-by: zaklina <zdata@oroinc.com>

---
+++ src/TC/Bundle/ProductBundle/EventListener/OrderListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace TC\Bundle\ProductBundle\EventListener;
+
+use Oro\Bundle\OrderBundle\Entity\Order;
+use Oro\Bundle\ProductBundle\Search\Reindex\ProductReindexManager;
+
+class OrderListener
+{
+    public function __construct(private ProductReindexManager $productReindexManager)
+    {
+    }
+
+    public function postPersist(Order $order): void
+    {
+        $this->reindexProducts($order);
+    }
+
+    public function postRemove(Order $order,): void
+    {
+        $this->reindexProducts($order);
+    }
+
+    private function reindexProducts(Order $order): void
+    {
+        $productIds = [];
+        foreach ($order->getLineItems() as $lineItem) {
+            $productIds[] = $lineItem->getProduct()->getId();
+        }
+        $this->productReindexManager->reindexProducts($productIds);
+    }
+}
