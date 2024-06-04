ff95a4b386a4759d5200522298a67bef7c15bb73
veremenko <veremenko@oroinc.com>
2024-06-05 00:52:08 +0300

    - add bundle with exercise

--- src/Demo/Bundle/OrderBundle/DemoOrderBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Demo\Bundle\OrderBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DemoOrderBundle extends Bundle
-{
-}
---
+++ src/Training/Bundle/NewThemeBundle/TraningNewThemeBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Training\Bundle\NewThemeBundle;
+
+use Demo\Bundle\AcmeThemeBundle\DependencyInjection\DemoAcmeThemeExtension;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TrainingNewThemeBundle extends Bundle
+{
+}
