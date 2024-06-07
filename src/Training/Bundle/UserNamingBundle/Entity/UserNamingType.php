ee65ff49d32016a8cb54acee67e0c3e815e30223
Yevhen Shyshkin <yshyshkin@oroinc.com>
2024-06-07 16:27:54 +0200

    Step 2: Entities and Migrations

---
+++ src/Training/Bundle/UserNamingBundle/Entity/UserNamingType.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Training\Bundle\UserNamingBundle\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Oro\Bundle\EntityConfigBundle\Metadata\Attribute\Config;
+use Oro\Bundle\EntityExtendBundle\Entity\ExtendEntityInterface;
+use Oro\Bundle\EntityExtendBundle\Entity\ExtendEntityTrait;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'training_user_naming_type')]
+#[Config(
+    defaultValues: [
+        'entity' => ['icon' => 'fa-child']
+    ]
+)]
+class UserNamingType implements ExtendEntityInterface
+{
+    use ExtendEntityTrait;
+
+    #[ORM\Id]
+    #[ORM\Column(type: Types::INTEGER)]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    private ?int $id = null;
+
+    #[ORM\Column(name: 'title', type: Types::STRING, length: 64, nullable: false)]
+    private ?string $title = null;
+
+    /**
+     * Allowed placeholders are: PREFIX, FIRST, MIDDLE, LAST, SUFFIX
+     */
+    #[ORM\Column(name: 'format', type: Types::STRING, length: 255, nullable: false)]
+    private ?string $format = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * @param string $format
+     * @return $this
+     */
+    public function setFormat(string $format): self
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+}
