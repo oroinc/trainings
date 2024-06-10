10d4573022126729adb7127c9e6a47a0b4ab0f27
Yevhen Shyshkin <yshyshkin@oroinc.com>
2024-06-10 15:57:05 +0200

    Step 5: Security and Access Control Lists

--- src/Training/Bundle/UserNamingBundle/Controller/UserNamingController.php
+++ src/Training/Bundle/UserNamingBundle/Controller/UserNamingController.php
@@ -2,6 +2,8 @@
 
 namespace Training\Bundle\UserNamingBundle\Controller;
 
+use Oro\Bundle\SecurityBundle\Attribute\Acl;
+use Oro\Bundle\SecurityBundle\Attribute\AclAncestor;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Training\Bundle\UserNamingBundle\Entity\UserNamingType;
@@ -10,6 +12,7 @@ class UserNamingController
 {
     #[Route(path: '/', name: 'training_user_naming_index')]
     #[Template]
+    #[AclAncestor('training_user_naming_view')]
     public function indexAction(): array
     {
         return [
@@ -23,6 +26,7 @@ class UserNamingController
      */
     #[Route(path: '/view/{id}', name: 'training_user_naming_view', requirements: ['id' => '\d+'])]
     #[Template]
+    #[Acl(id: 'training_user_naming_view', type: 'entity', class: UserNamingType::class, permission: 'VIEW')]
     public function viewAction(UserNamingType $type): array
     {
         return [
