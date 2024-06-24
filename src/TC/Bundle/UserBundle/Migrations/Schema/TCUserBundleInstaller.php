d69d01d8b30acd2e61c71439876bbe7e91b19d29
zaklina <zdata@oroinc.com>
2024-06-24 11:40:37 +0200

    EDU-50 Message Queue - moving less important calculations to MQ - minor changes

--- src/TC/Bundle/UserBundle/Migrations/Schema/TCUserBundleInstaller.php
+++ src/TC/Bundle/UserBundle/Migrations/Schema/TCUserBundleInstaller.php
@@ -5,20 +5,11 @@ namespace TC\Bundle\UserBundle\Migrations\Schema;
 use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\EntityBundle\EntityConfig\DatagridScope;
 use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
-use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
-use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Installation;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
-class TCUserInstaller implements Installation, ExtendExtensionAwareInterface
+class TCUserBundleInstaller implements Installation
 {
-    protected ExtendExtension $extendExtension;
-
-    public function setExtendExtension(ExtendExtension $extendExtension)
-    {
-        $this->extendExtension = $extendExtension;
-    }
-
     public function getMigrationVersion()
     {
         return 'v1_0';
@@ -40,8 +31,8 @@ class TCUserInstaller implements Installation, ExtendExtensionAwareInterface
                 'length' => 255,
                 'oro_options' => [
                     'extend' => ['owner' => ExtendScope::OWNER_CUSTOM],
-                    'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_HIDDEN],
-                    'form' => ['is_enabled' => false]
+                    'form' => ['is_enabled' => false],
+                    'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_HIDDEN]
                 ],
             ]
         );
