0f89e9b3e47e05b9ef0d96d18f35d8993d3f7eba
Alexandr Zhulev <azhulev@oroinc.com>
2024-06-25 15:56:35 +0300

    OTTPC-206: Training preparation - updates

--- src/Training/Bundle/UserNamingBundle/ImportExport/Configuration/UserNamingImportExportConfigurationProvider.php
+++ src/Training/Bundle/UserNamingBundle/ImportExport/Configuration/UserNamingImportExportConfigurationProvider.php
@@ -16,9 +16,9 @@ class UserNamingImportExportConfigurationProvider implements ImportExportConfigu
     {
         return new ImportExportConfiguration([
             ImportExportConfiguration::FIELD_ENTITY_CLASS => UserNamingType::class,
+            ImportExportConfiguration::FIELD_IMPORT_PROCESSOR_ALIAS => 'training_user_naming_type_add_or_replace',
             ImportExportConfiguration::FIELD_EXPORT_PROCESSOR_ALIAS => 'training_user_naming_type',
             ImportExportConfiguration::FIELD_EXPORT_TEMPLATE_PROCESSOR_ALIAS => 'training_user_naming_type',
-            ImportExportConfiguration::FIELD_IMPORT_PROCESSOR_ALIAS => 'training_user_naming_type_add_or_replace'
         ]);
     }
 }
