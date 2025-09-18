20f7ad6a88cb384263e71ad0f00195c12abd4c56
Alexandr Zhulev <azhulev@oroinc.com>
2025-09-18 15:10:04 +0300

    OTTPC-248: FE Training Themes and Layouts. - removed redundant bundles

--- src/Acme/Bundle/ShoppinglistBadgeBundle/AcmeShoppinglistBadgeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Acme\Bundle\ShoppinglistBadgeBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AcmeShoppinglistBadgeBundle extends Bundle
-{
-}
--- src/Training/Bundle/FrontendTrainingBundle/TrainingFrontendTrainingBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Training\Bundle\FrontendTrainingBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TrainingFrontendTrainingBundle extends Bundle
-{
-}
